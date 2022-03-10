b1162226af1ab41e5042b89178e4c4b7c93540e3
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2022-03-11 00:09:26 +0900

    fix: remove header

--- packages/client/src/lib/api.ts
+++ packages/client/src/lib/api.ts
@@ -7,7 +7,4 @@ export const handleApiError = (error: Error | ApiError) => {
 
 export const apiClient = new ApiClient({
   BASE: import.meta.env.VITE_APP_API_BASE_URL,
-  HEADERS: {
-    "Access-Control-Allow-Origin": "no-cors",
-  },
 });
