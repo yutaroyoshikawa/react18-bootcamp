fb5202b1a786afe5d5d782d96301d2cdd0c1a2c5
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2022-03-10 12:28:40 +0900

    feat: install router

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,17 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import { useThemeClass } from "../src/features/appSettings/modules/themeHooks";
 import { globalStyles } from "../src/style";
+import { Home } from "./pages/index";
+
+const routes: { path: string; element: JSX.Element }[] = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+];
 
 export const App: FC = () => {
-  const [count, setCount] = useState(0);
   const themeClass = useThemeClass();
 
   useEffect(() => {
@@ -12,36 +20,11 @@ export const App: FC = () => {
 
   return (
     <div className={themeClass}>
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Routes>
+        {routes.map((route) => (
+          <Route {...route} key={route.path} />
+        ))}
+      </Routes>
     </div>
   );
 };
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
