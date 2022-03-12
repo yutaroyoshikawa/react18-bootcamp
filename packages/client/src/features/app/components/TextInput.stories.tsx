3ad2b8746e007f74bb9f5da4365248b7a5a7a952
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2022-03-12 23:57:47 +0900

    feat: TextInput コンポーネント追加

---
+++ packages/client/src/features/app/components/TextInput.stories.tsx
@@ -0,0 +1,14 @@
+import type { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { TextInput } from "./TextInput";
+
+const meta: ComponentMeta<typeof TextInput> = {
+  component: TextInput,
+};
+
+export default meta;
+
+export const Base: ComponentStoryObj<typeof TextInput> = {
+  args: {
+    placeholder: "テキスト",
+  },
+};
