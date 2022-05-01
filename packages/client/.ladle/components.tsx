31f035e1fab55d55c2a7dec124795667f8b76082
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2022-05-02 01:59:45 +0900

    chore: storybook アンインストール

--- packages/client/.ladle/components.tsx
+++ packages/client/.ladle/components.tsx
@@ -1,13 +1,21 @@
 import type { GlobalProvider } from "@ladle/react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { MemoryRouter } from "react-router-dom";
+import { useThemeClass } from "../src/features/app/modules/themeHooks";
 import { enableIcon } from "../src/lib/icon";
 import { globalStyles } from "../src/lib/style";
 
 globalStyles();
 enableIcon();
 
-export const Provider: GlobalProvider = ({ children }) => (
-  <>
-    <MemoryRouter>{children}</MemoryRouter>
-  </>
-);
+export const Provider: GlobalProvider = ({ children }) => {
+  const className = useThemeClass();
+
+  return (
+    <HelmetProvider>
+      <div id="modalEl" />
+      <Helmet title="コミュニティ" htmlAttributes={{ class: className }} />
+      <MemoryRouter>{children}</MemoryRouter>
+    </HelmetProvider>
+  );
+};
